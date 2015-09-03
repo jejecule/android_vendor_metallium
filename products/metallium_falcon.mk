315cfe33e1b0ea0592b1dd56da7f0091371cfa89
MardonHH <markusmenzel@gmail.com>
2015-09-03 16:43:49 +0200

    Update metallium_falcon.mk

--- products/metallium_falcon.mk
+++ products/metallium_falcon.mk
@@ -1,10 +1,17 @@
-$(call inherit-product, device/motorola/falcon/full_falcon.mk)
+# Check for target product
+ifeq (metallium_d802,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := metallium_xhdpi
 
 # Inherit telephony common stuff
 $(call inherit-product, vendor/metallium/configs/telephony.mk)
 
 # Include metallium common configuration
 include vendor/metallium/main.mk
+
+# Inherit device configuration
+$(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
 PRODUCT_NAME := metallium_falcon
