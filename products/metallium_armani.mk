f0e58b0d2d8b1659ed70224d709c9ad3cfda79b9
MardonHH <markusmenzel@gmail.com>
2015-09-11 20:24:43 +0200

    Update metallium_armani.mk

--- products/metallium_armani.mk
+++ products/metallium_armani.mk
@@ -19,7 +19,7 @@ ifeq (metallium_armani,$(TARGET_PRODUCT))
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, 
-$(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(SRC_TARGET_DIR)/product/full_base_telephony.mk
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/xiaomi/armani/device.mk) $(call 
